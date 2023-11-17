457547a6527742cc02fb63cb58518300d1b264b8
Bismina <114130600+Bismina@users.noreply.github.com>
2023-11-17 12:32:50 +0530

    main

--- sun_travel/src/Components/BestSellingSection.jsx
+++ sun_travel/src/Components/BestSellingSection.jsx
@@ -6,23 +6,28 @@ import { FaIndianRupeeSign } from "react-icons/fa6";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import maldives from "./assets/maldives.jpg";
+import dubai from "./assets/duabi.jpg";
+import thailand from "./assets/Thailand.jpg";
 export default function BestSellingSection() {
   const HotSellTourDetails = [
     {
       id: 1,
-      image: "https://picsum.photos/200/300",
+      image: maldives ,
       place: "Maldives Paradise",
-      days: " 7 Days",
-      price: 2500,
-      offerType: "bestseller",
+      days: "5 Days",
+      price: 80000,
+      offerType: "our best seller",
       description: "Relax in the stunning beaches of Maldives for 7 days.",
     },
     {
       id: 2,
-      image: "https://picsum.photos/200/300",
-      place: "Thai Adventure",
+      image: thailand,
+      place: "Thailand",
       days: "10 Days",
-      price: 1800,
+      price: 10000,
       offerType: "20%offer",
       description: "Explore the adventurous side of Thailand for 10 days.",
     },
@@ -50,7 +55,7 @@ export default function BestSellingSection() {
       place: "Malaysian Marvels",
       days: "8 Days",
       price: 2000,
-      offerType: "bestseller",
+      offerType: "our best seller",
       description: "Marvel at the wonders of Malaysia for 8 days.",
     },
     {
@@ -86,7 +91,7 @@ export default function BestSellingSection() {
       place: "Golden Triangle & Himachal",
       days: "9 Days",
       price: 1900,
-      offerType: "bestseller",
+      offerType: "our best seller",
       description: "Discover the Golden Triangle and Himachal for 9 days.",
     },
     {
@@ -98,15 +103,58 @@ export default function BestSellingSection() {
       offerType: "20% offer",
       description: "Experience the serene backwaters of Kerala for 6 days.",
     },
+    {
+      id:11,
+      image:dubai,
+      place:"Dubai Group Tour",
+      days:"5 Days",
+      price:28000,
+      offerType:"Combo offer",
+      description:"Experience the serene backwaters of Dubai for 6 days"
+
+    }
   ];
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4.5,
-    slidesToScroll: 1,
-    autoplay: true, // Enable autoplay
-    autoplaySpeed: 2000, // Set the autoplay speed in milliseconds
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 1599, min: 1025 },
+      items: 5,
+      paritialVisibilityGutter: 20,
+    },
+    desktopLarge: {
+      breakpoint: { max: 3000, min: 1600 },
+      items: 6,
+      paritialVisibilityGutter: 20,
+    },
+    tabletLarge: {
+      breakpoint: { max: 1024, min: 768 },
+      items: 5,
+      paritialVisibilityGutter: -10,
+    },
+    tablet: {
+      breakpoint: { max: 767, min: 576 },
+      items: 4,
+      paritialVisibilityGutter: -50,
+    },
+    tabletSmall: {
+      breakpoint: { max: 575, min: 425 },
+      items: 3,
+      paritialVisibilityGutter: -60,
+    },
+    mobileLarge: {
+      breakpoint: { max: 424, min: 375 },
+      items: 2,
+      paritialVisibilityGutter: -20,
+    },
+    mobile: {
+      breakpoint: { max: 374, min: 321 },
+      items: 1,
+      paritialVisibilityGutter: 60,
+    },
+    mobileSmall: {
+      breakpoint: { max: 320, min: 0 },
+      items: 1,
+      paritialVisibilityGutter: 50,
+    },
   };
   return (
     <Wrapper>
@@ -114,67 +162,173 @@ export default function BestSellingSection() {
         <div className="headingLabel">
           <span>Hot-Selling </span>Tour Bundles
         </div>
-        <SplittingDiv>
-          <CarouselWrapper>
-            <Slider {...settings}>
-              {HotSellTourDetails.map((item) => {
-                return (
-                  <>
-                    <CardSectionBesSelling className="card">
-                      <img src={item.image} alt="" />
-
-                      <div className="intro">
-                        <h1>{item.place}</h1>
-                        <p className="para">{item.description}</p>
-                        <DayWrapper className="day">
-                          <ion-icon name="today-outline"></ion-icon>
-                          <div>{item.days}</div>
-                          <MoneyWrapper>
-                            <FaIndianRupeeSign style={{ color: "red" }} />
-                            <div>{item.price}</div>
-                          </MoneyWrapper>
-                        </DayWrapper>
-                      </div>
-                    </CardSectionBesSelling>
-                  </>
-                );
-              })}
-            </Slider>
-          </CarouselWrapper>
-        </SplittingDiv>
       </HotSellingSection>
+
+      <Carousel
+        responsive={responsive}
+        arrows={true}
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={2000}
+        customTransition="transform 500ms ease-in-out"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        itemClass="carousel-item"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        deviceType="desktop"
+        sliderClass="carousel-slider"
+        dotListClass="custom-dot-list-style"
+        renderButtonGroupOutside={true}
+        renderDotsOutside={false}
+        slidesToSlide={1}
+        swipeable={true}
+        partialVisible={true}
+        showDots={false}
+        ssr={true}
+        draggable={true}
+        // beforeChange={handleBeforeChange}
+        // afterChange={handleAfterChange}
+      >
+        {HotSellTourDetails?.map((item, id) => {
+          return (
+            <>
+              <ContentWrapper
+                // key={id}
+                // onClick={e => handleDetails(e, item?.id, 'Announcement')}
+                className="other-ContentWrapper"
+              >
+                <div className="cardWrapper">
+                  <ImgStyle
+                    className="image-style"
+                    src={item.image}
+                    alt={item.image}
+                  />
+                  <div className="boxShadow"></div>
+                </div>
+                <TagImage
+                  className={
+                    item.offerType === "our best seller" ? "greenTag" : "redTag"
+                  }
+                >
+                  {item.offerType}
+                </TagImage>
+                <TextWrapper className="text-style">
+                  <div style={{ textAlign: "left" }}>
+                    <AnnouncemnetTitle className="announcemnetTitle">
+                      {item.place}
+                    </AnnouncemnetTitle>
+
+                    <div className="hidingData">
+                      <DateDiv>{item.description}</DateDiv>
+
+                      <DayWrapper className="day">
+                        <ion-icon name="today-outline"></ion-icon>
+                        <div>{item.days}</div>
+                        <MoneyWrapper>
+                          <FaIndianRupeeSign style={{ color: "red" }} />
+                          <div>{item.price}</div>
+                        </MoneyWrapper>
+                      </DayWrapper>
+                    </div>
+                  </div>
+                </TextWrapper>
+              </ContentWrapper>
+            </>
+          );
+        })}
+      </Carousel>
+
+      <CardView className="otherNews-CardView"></CardView>
     </Wrapper>
   );
 }
-const CarouselWrapper = styled.div`
-  width: 98%; /* Adjust the width as needed */
-  /* margin: auto; */
-  /* overflow-y: hidden; */
-`;
-
-const MoneyWrapper = styled.div`
+const CardView = styled.div`
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+  position: relative;
+  /* background: ${(p) => p.theme.cardBgTheme}; */
+  /* background-color: ${(p) => p.theme.cardBgTheme}; */
+  border-radius: 0 0 20px 20px;
+  /* padding: 25px; */
   display: flex;
-  justify-content: flex-start;
+  justify-content: stretch;
   align-items: center;
-  gap: 2px;
+  gap: 30px;
 `;
-const DayWrapper = styled.div`
-  padding: 10px;
-  font-size: 14px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 7px;
-  ion-icon {
-    font-size: 15px;
-    color: #001499;
+const DateDiv = styled.div`
+  color: #fff;
+  font-size: 12px;
+  margin-top: 3px;
+  text-transform: capitalize;
+  /* letter-spacing: 1px; */
+  padding-left: 10px;
+`;
+const ContentWrapper = styled.div`
+  cursor: pointer;
+  &:hover {
+    .text-style {
+      color: green;
+      transform: translateY(-100%);
+    }
+    .hidingData {
+      opacity: 1;
+      visibility: visible;
+      transition: transform 0.3s ease;
+    }
   }
+  .hidingData {
+    opacity: 0;
+    visibility: hidden;
+  }
+  position: relative;
+  .boxShadow {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(
+      to top,
+      rgb(0 0 0 / 97%) 0,
+      rgb(197 191 191 / 9%) 60%,
+      rgb(255 253 255 / 0%) 100%
+    );
+  }
+  .cardWrapper {
+    max-width: 250px;
+    width: 250px;
+    /* background: red; */
+    height: 300px;
+    max-height: 300px;
+    overflow: hidden;
+    border-radius: 15px;
+    position: relative;
+    /* cursor: pointer; */
+    &:hover .image-style {
+      transform: scale(1.1);
+    }
+  }
+  /* background-color: red; */
+  /* width: 100%; */
+  /* display: flex; */
+  /* padding-bottom: 40px; */
+  /* align-items: center; */
+  position: relative;
+  /* &:nth-last-child(1) {
+    padding-bottom: 10px;
+    b
+  }  */
 `;
-const SplittingDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 35px;
+const ImgStyle = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+  object-fit: cover;
+
+  transition: transform 0.3s ease;
 `;
 const Wrapper = styled.div`
   height: 100vh;
@@ -182,8 +336,21 @@ const Wrapper = styled.div`
 
   position: relative;
 `;
+const AnnouncemnetTitle = styled.div`
+  text-transform: uppercase;
+  font-family: 20px;
+  color: #fff;
+  font-family: "Lato";
+  font-weight: 600;
+  word-spacing: 3px;
+  /* text-align: left; */
+  padding-left: 10px;
+`;
 const HotSellingSection = styled.div`
-/* margin: 10px; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* margin: 10px; */
   .headingLabel {
     font-family: "Lato";
     font-size: 40px;
@@ -196,6 +363,35 @@ const HotSellingSection = styled.div`
     }
   }
 `;
+const TextWrapper = styled.div`
+  position: absolute;
+  bottom: -33px;
+  transition: transform 0.5s ease;
+  /* padding-left: 10px; */
+  width: 200px;
+`;
+const TagImage = styled.div`
+  &.greenTag {
+    background-color: #1ca749;
+  }
+
+  &.redTag {
+    background-color: #ff0000;
+  }
+  text-transform: uppercase;
+  border-radius: 3px;
+  color: #fff;
+  padding: 5px 15px;
+  position: absolute;
+  top: 20px;
+  left: 0px;
+  display: inline;
+  font-size: 13px;
+  font-family: "Lato";
+  font-weight: 600;
+  margin-left: 5px;
+  letter-spacing: 1px;
+`;
 const CardSectionBesSelling = styled.div`
   /* margin-top: 10px; */
 
@@ -204,7 +400,7 @@ const CardSectionBesSelling = styled.div`
   img {
     min-height: 380px;
     height: 100%;
-    background-color: red;
+    /* background-color: red; */
     margin-top: 15px;
     background: #fff;
     box-shadow: #00000063 0px 0px 22px;
@@ -269,39 +465,19 @@ const CardSectionBesSelling = styled.div`
     visibility: "hidden";
   }
 `;
-const ReadMore = styled.div`
-  position: absolute;
-  bottom: 350px;
-  font-size: 12px;
-  background-color: red;
-  border-radius: 1px solid blue;
-  right: 11px;
-  padding: 7px;
-  border-radius: 6px;
+const DayWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
   color: #fff;
-  font-family: "Lato";
+  margin-left: 10px;
+  gap: 4px;
+  margin-top: 10px;
 `;
-const BestSeller = styled.div`
-  font-size: 12px;
-  font-family: "Lato";
-  background-color: #1ca749;
-  padding: 5px 26px;
-  width: 90px;
-  position: absolute;
-  top: 26px;
-  left: -13px;
-  height: 20px;
-  z-index: 1000;
-  /* position: relative; */
-  span {
-    display: block;
-    background-color: #fec100;
-    width: 14px; /* Width of the yellow section */
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    border-radius: 1px 3px 3px 1px;
-  }
+const MoneyWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-left: 10px;
+  /* gap: 4px; */
 `;
